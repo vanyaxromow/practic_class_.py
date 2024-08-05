f54d649f579703009cc989cfd1bd4417f231a88a
vanyaxromow <vanya.xromow@yandex.ru>
2024-08-05 13:29:34 +0300

    lesson3

--- main.py
+++ main.py
@@ -20,9 +20,23 @@ class User:
 if __name__ == '__main__':
     database = Database()
     while True:
-        choise = input('Приветствую! Выберите действие: \n 1 - Вход \n 2 - Регистрация\n')
-        user = User(input('Введите логин: '), password := input('Введите пароль: '), password2 := input('Повторите пароль: '))
-        if password != password2:
-            exit()
-        database.add_user(user.username, user.password)
+        choise = int(input('Приветствую! Выберите действие: \n 1 - Вход \n 2 - Регистрация\n'))
+        if choise == 1:
+            login = input('Введите логин: ')
+            password = input('Введите пароль: ')
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Вход выполнен, {login}')
+                    break
+                else:
+                    print('Неверный пароль')
+            else:
+                print('Пользователь не найден.')
+        if choise == 2:
+            user = User(input('Введите логин: '), password := input('Введите пароль: '),
+                        password2 := input('Повторите пароль: '))
+            if password != password2:
+                print('Пароли не совпадают, попробуйте еще раз')
+                continue
+            database.add_user(user.username, user.password)
         print(database.data)
